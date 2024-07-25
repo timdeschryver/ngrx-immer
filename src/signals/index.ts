e1b34dadbc4da804f13bef954ddb6ec06c4747e2
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2024-07-25 19:19:39 +0200

    feat(signals): update to @ngrx/signals 18
    BREAKING CHANGE:
    
    Requires NgRx Signals v18 to be compatible with this version.

--- src/signals/index.ts
+++ src/signals/index.ts
@@ -1,4 +1,4 @@
-import { PartialStateUpdater, patchState, StateSignal } from '@ngrx/signals';
+import { PartialStateUpdater, patchState, WritableStateSource,  Prettify } from '@ngrx/signals';
 import { immerReducer } from 'ngrx-immer';
 
 export type ImmerStateUpdater<State extends object> = (state: State) => void;
@@ -12,9 +12,7 @@ function toFullStateUpdater<State extends object>(updater: PartialStateUpdater<S
 		return;
 	};
 }
-
-type Prettify<T> = { [K in keyof T]: T[K] } & {};
-export function immerPatchState<State extends object>(state: StateSignal<State>, ...updaters: Array<Partial<Prettify<State>> | PartialStateUpdater<Prettify<State>> | ImmerStateUpdater<Prettify<State>>>) {
+export function immerPatchState<State extends object>(state: WritableStateSource<State>, ...updaters: Array<Partial<Prettify<State>> | PartialStateUpdater<Prettify<State>> | ImmerStateUpdater<Prettify<State>>>): void {
 	const immerUpdaters = updaters.map(updater => {
 		if (typeof updater === 'function') {
 			return immerReducer(toFullStateUpdater(updater)) as unknown as PartialStateUpdater<State & {}>;
